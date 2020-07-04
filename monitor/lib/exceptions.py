fa4f8a95b846755056a6cf18f40c6674ff9acc36
Daniel Weiner <info@phantomnet.net>
2020-07-04 01:00:43 -0700

    Added a config parser for handling INI based configuration

---
+++ monitor/lib/exceptions.py
@@ -0,0 +1,9 @@
+class MessageError(Exception):
+    message = 'Whoops! Something went wrong'
+
+    def __init__(self, message=None, *args, **kwargs):
+        self.message = message or self.message
+        if args:
+            self.message = message.format(*args)
+        elif kwargs:
+            self.message = message.format(**kwargs)
