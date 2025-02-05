04ed1a609afd5e6906aec7c394b199ee3ebe587c
Daniel Weiner <info@phantomnet.net>
2025-02-04 17:11:36 -0800

    Updated setup script to handle dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import setuptools
 
 try:
@@ -7,12 +8,22 @@ except IOError as e:
     print('Failed to read README file: {}'.format(e))
     exit(1)
 
+def ReadRequirements(path):
+    requirements = []
+    if os.path.isfile(path):
+        with open(path, encoding='utf-8') as handle:
+            for line in handle:
+                line = line.strip()
+                if len(line) > 0 and not line.startswith('#'):
+                    requirements.append(line)
+    return requirements
+
 # Packaging tutorial: https://packaging.python.org/tutorials/packaging-projects/
 # Package classifiers: https://pypi.org/classifiers/
 
 setuptools.setup(
     name='PyMonitorLib',
-    version='0.4.0',
+    version='0.4.1',
     author='Daniel Weiner',
     author_email='info@phantomnet.net',
     description='Library for creating simple interval processes. This is especially '
@@ -22,6 +33,8 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/Aralocke/PyMonitorLib',
     packages=setuptools.find_packages(),
+    install_requires=ReadRequirements('requirements.txt'),
+    extras_require={'dev': ReadRequirements('dev-requirements.txt')},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
